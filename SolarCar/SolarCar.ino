c8e33aa9b39330e1523932bba11ec741920b766b
moayman <mo_ayman@live.com>
2015-03-16 23:31:53 +0200

    Used Serial.write instead of Serial.print

---
+++ SolarCar/SolarCar.ino
@@ -0,0 +1,23 @@
+byte incomingByte;
+byte steeringAngle;
+bool RightLeft;
+bool forward;
+bool backward;
+
+void setup() 
+{
+  Serial.begin(38400);
+}
+
+void loop() 
+{	
+  if (Serial.available() > 0) 
+  {
+    incomingByte = Serial.read();
+    RightLeft = (incomingByte >> 4) & 1;
+    steeringAngle = (incomingByte & 0x0C) >> 2;
+    forward = incomingByte & 1;
+    backward = (incomingByte >> 1) & 1;
+    Serial.write(incomingByte);
+  }    
+}
