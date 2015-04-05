212d0c1c3738d58031cf8cf66f778ae4f248c0ff
moayman <mo_ayman@live.com>
2015-04-06 00:07:01 +0200

    Completely Redesigned
    -Added Servo support
    -Added Angles array
    -Right and Left are now a single Motor

--- SolarCar/SolarCar.ino
+++ SolarCar/SolarCar.ino
@@ -1,17 +1,21 @@
-#define CH1DIR 2
-#define CH1PWM 3
-#define CH2DIR 4
-#define CH2PWM 5
+#include <Servo.h> 
 
+#define DIR 2
+#define PWM 3
+#define STEERINGMOTOR 5
+
+Servo Steering;
 byte received;
-byte right;
-byte left;
-bool backward;
+byte Angle;
+byte Speed;
+byte oldAngle;
 
-int Speeds[] = { 0, 35, 70, 100, 150, 180, 220, 254};
+int Speeds[] = { 254, 220, 180, 150, 100, 70, 35, 0, 35, 70, 100, 150, 180, 220, 254};
+int Angles[] = { -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35};
 
 void setup() 
 {
+  Steering.attach(STEERINGMOTOR);
   Serial.begin(9600);
 }
 
@@ -20,13 +24,16 @@ void loop()
   if (Serial.available() > 0) 
   {
     received = Serial.read();
-    backward = (received >> 7) & 1;
-    right = received & 0x07;
-    left = (received & 0x38) >> 3;
-    digitalWrite(CH1DIR,!backward);
-    digitalWrite(CH2DIR,!backward);
+    Speed = received & 0x0F;
+    oldAngle = Angle;
+    Angle = received >> 4;
+    if(Speed < 6)
+      digitalWrite(DIR,LOW);
+    else
+      digitalWrite(DIR,HIGH);
+    if(Angle != oldAngle)
+      Steering.write(Angles[Angle]);
     Serial.write(received);
   }
-  analogWrite(CH1PWM,Speeds[right]);
-  analogWrite(CH2PWM,Speeds[left]);
+  analogWrite(PWM,Speeds[Speed]);
 }
