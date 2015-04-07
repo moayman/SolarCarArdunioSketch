a39323e465e583cdc737193ed568e089a2059736
moayman <mo_ayman@live.com>
2015-04-08 01:08:53 +0200

    Steering Offset

--- SolarCar/SolarCar.ino
+++ SolarCar/SolarCar.ino
@@ -12,13 +12,25 @@ byte oldAngle;
 
 int Speeds[] = { 254, 220, 180, 150, 100, 70, 35, 0, 35, 70, 100, 150, 180, 220, 254};
 int Angles[] = { -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35};
+int Offset=90;
 
 void setup() 
 {
   Steering.attach(STEERINGMOTOR);
   Serial.begin(9600);
 }
-
+void MoveServo(int Target)
+{
+    int cpos=Steering.read();
+    int dir=Target>cpos?1:-1;
+    for(cpos;cpos!=Target;cpos+=dir)
+    {
+        Steering.write(cpos);
+        delay(2);
+    }
+    Steering.write(cpos);
+    delay(2);
+}
 void loop() 
 {	
   if (Serial.available() > 0) 
@@ -31,8 +43,10 @@ void loop()
       digitalWrite(DIR,LOW);
     else
       digitalWrite(DIR,HIGH);
-    if(Angle != oldAngle)
-      Steering.write(Angles[Angle]);
+    MoveServo(Angles[Angle]+Offset);
+    //if(Angle != oldAngle)
+      
+      //Steering.write(Angles[Angle]);
     Serial.write(received);
   }
   analogWrite(PWM,Speeds[Speed]);
